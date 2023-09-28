acfc76a084a52b66fa1c80610468130ee90bcdd5
Phil Howard <github@gadgetoid.com>
2023-09-28 17:38:14 +0100

    libgpiod: v1.2.1

--- setup.py
+++ setup.py
@@ -19,49 +19,42 @@ class build_ext(orig_build_ext):
         super().run()
         rmtree(path.join(self.build_lib, "tests"), ignore_errors=True)
 
-with open("gpiod/version.py", "r") as fd:
-    exec(fd.read())
+try:
+    with open("gpiod/version.py", "r") as fd:
+        exec(fd.read())
+except IOError:
+    __version__ = open("libgpiod/NEWS", "r").readline()[10:].strip()
 
 sources = [
-        "gpiod/ext/chip.c",
-        "gpiod/ext/common.c",
-        "gpiod/ext/line-config.c",
-        "gpiod/ext/line-settings.c",
-        "gpiod/ext/module.c",
-        "gpiod/ext/request.c",
+        "gpiod/chip.cpp",
+        "gpiod/iter.cpp",
+        "gpiod/line_bulk.cpp",
+        "gpiod/line.cpp",
+        "libgpiod/bindings/python/gpiodmodule.c"
 ]
 
 sources += glob.glob("libgpiod/lib/*.c")
 
 gpiod_ext = Extension(
-    "gpiod._ext",
+    "gpiod",
     sources=sources,
     define_macros=[("_GNU_SOURCE", "1")],
-    include_dirs=["libgpiod/include", "libgpiod/lib", "gpiod/ext"],
-    extra_compile_args=["-Wall", "-Wextra", "-DGPIOD_VERSION_STR=\"{}\"".format(__version__)],
+    include_dirs=["libgpiod/include", "libgpiod/lib", "gpiod"],
+    extra_compile_args=["-Wall", "-Wextra", "-DGPIOD_VERSION_STR=\"{}\"".format(__version__)]
 )
 
 gpiosim_ext = Extension(
     "tests.gpiosim._ext",
-    sources=["tests/gpiosim/ext.c"],
+    sources=["tests/gpiomockupmodule.c"],
     define_macros=[("_GNU_SOURCE", "1")],
     libraries=["gpiosim"],
     include_dirs=["include"],
     extra_compile_args=["-Wall", "-Wextra"],
 )
 
-procname_ext = Extension(
-    "tests.procname._ext",
-    sources=["tests/procname/ext.c"],
-    include_dirs=["include"],
-    define_macros=[("_GNU_SOURCE", "1")],
-    extra_compile_args=["-Wall", "-Wextra"],
-)
-
 extensions = [gpiod_ext]
 if "GPIOD_WITH_TESTS" in environ and environ["GPIOD_WITH_TESTS"] == "1":
     extensions.append(gpiosim_ext)
-    extensions.append(procname_ext)
 
 setup(
     name="libgpiod",
